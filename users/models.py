b73c03c6682ed9c1525bad07c6fcda3cc80cee8d
Ibukun <ibk2k7@gmail.com>
2022-09-22 15:06:57 +0100

    changed address length

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ GENDER_SELECTION = [
 
 class NewUser(AbstractUser):
     phone = models.CharField(max_length=20, blank=True, null=True)
-    address = models.CharField(max_length=100, blank=True, null=True)
+    address = models.CharField(max_length=400, blank=True, null=True)
     gender = models.CharField(
         max_length=20, choices=GENDER_SELECTION, blank=True, null=True
     )
